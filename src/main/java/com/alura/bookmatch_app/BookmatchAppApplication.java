8f76a922448ee395d1e09941907da9418509b760
DevRys <eduardoreyes55@aragon.unam.mx>
2024-07-06 16:56:56 -0600

    first commit

---
+++ src/main/java/com/alura/bookmatch_app/BookmatchAppApplication.java
@@ -0,0 +1,13 @@
+package com.alura.bookmatch_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmatchAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/bookmatch_app/BookmatchAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.bookmatch_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmatchAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
